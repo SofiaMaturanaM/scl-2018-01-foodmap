477fb15ab7cc86e72331e6ec33d40faefa61f7a7
SofiaMaturanaM <sofimatsu@gmail.com>
2018-07-26 13:43:19 -0400

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,8 @@ window.onload = function getLocation() {
 function showPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
+    console.log(latitude)
+    console.log(longitude)
 }
 
 function showError(error) {
@@ -27,31 +29,6 @@ function showError(error) {
             break;
     }
 }
-
-
-
-
-let restaurantPoint = (object) => {
-    // let id = object.response.data.objeto_id;
-    // console.log(id)
-    fetch(`https://developers.zomato.com/api/v2.1/search?apikey=5f5a913462093a0dd31e6abfbc3022f1&entity_id=83&entity_type=city&q=${object}`)
-        .then((response) => response.json())
-        .then((responseJson) => {
-            // $('#mostrarMap').on('click', function() {
-            //     mostrarMapa(responseJson.response.data);
-            //     console.log(id)
-
-            // });
-            // responseJson.response.data.forEach(element => {
-            //     console.log(element);
-            // });
-            console.log(responseJson)
-        })
-        .catch((error) => {
-            console.log(error)
-        });
-}
-restaurantPoint()
 
 mostrarMapa = (puntos) => {
     var options = {
@@ -72,7 +49,7 @@ mostrarMapa = (puntos) => {
         const mymap = L.map('map').setView([latitud, longitud], 50);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3V0ZWZjbyIsImEiOiJjampjdXlqaG0zbXR3M3FvaGU5cG93ZXdlIn0.6b_Z9AeAgi1U7J4wiiX87w', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
+            maxZoom: 17,
             id: 'mapbox.streets',
             accessToken: 'your.mapbox.access.token'
         }).addTo(mymap);
@@ -96,3 +73,33 @@ mostrarMapa = (puntos) => {
 
 // fetch(`curl -X GET --header "Accept: application/json" --header "user-key: 5f5a913462093a0dd31e6abfbc3022f1" "https://developers.zomato.com/api/v2.1/search?count=50&lat=-33.4188152&lon=-70.64228059999999&radius=300&cuisines=4%2C151%2C3%2C159%2C168%2C30%2C229%2C25%2C153%2C391%2C40%2C45%2C134%2C156%2C181%2C143%2C169%2C148%2C55%2C60%2C67%2C70%2C300%2C73%2C162%2C82%2C304%2C177%2C641&sort=real_distance&order=asc`")
 //     .then(())
+
+let typeOfFood = document.getElementsByClassName("dropdown-item").value
+
+function filterByTypeOfFood() {
+
+
+}
+
+
+let restaurantPoint = (object) => {
+    // let id = object.response.data.objeto_id;
+    // console.log(id)
+    fetch(`https://developers.zomato.com/api/v2.1/search?apikey=5f5a913462093a0dd31e6abfbc3022f1&entity_id=83&entity_type=city&q=${object}`)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            // $('#mostrarMap').on('click', function() {
+            //     mostrarMapa(responseJson.response.data);
+            //     console.log(id)
+
+            // });
+            // responseJson.response.data.forEach(element => {
+            //     console.log(element);
+            // });
+            console.log(responseJson)
+        })
+        .catch((error) => {
+            console.log(error)
+        });
+}
+restaurantPoint()

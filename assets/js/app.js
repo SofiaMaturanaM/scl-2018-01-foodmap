66b59cba92d12bbe76f768010f65eddd50e6dbf9
SofiaMaturanaM <sofimatsu@gmail.com>
2018-07-26 21:05:41 -0400

    splash

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,53 +1,19 @@
-window.onload = function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.watchPosition(showPosition);
-    } else {
-        showError();
-    }
-}
-
-function showPosition(position) {
-    // let latitude = position.coords.latitude;
-    // let longitude = position.coords.longitude;
-    let latitude = -33.4188152;
-    let longitude = -70.64228059999999;
-    console.log(latitude)
-    console.log(longitude)
-}
-
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            alert("El usuario no da permiso para obtener su ubicación.")
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("La información de ubicación no está disponible.")
-            break;
-        case error.TIMEOUT:
-            alert("Ha expirado el tiempo de la solicitud.")
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("Ha ocurrido un error inesperado.")
-            break;
-    }
-}
-
 fetch(`https://developers.zomato.com/api/v2.1/search?apikey=5f5a913462093a0dd31e6abfbc3022f1&entity_id=83&entity_type=city&start=11&count=30&lat=-33.4188152&lon=-70.64228059999999&radius=200&cuisines=134%2C4%2C151%2C3%2C159%2C141%2C229%2C25%2C287%2C40%2C143%2C67%2C153%2C316%2C391%2C89%2C45%2C156%2C168%2C148%2C55%2C60%2C70%2C73%2C300%2C162%2C83%2C82%2C1005%2C304%2C177%2C95%2C641%2C99&sort=real_distance&order=desc`)
     .then(response => response.json())
     .then(data => {
 
-        consume(data);
+        // consume(data);
         console.log(data);
     })
 
-const consume = data => {
-    data.data.forEach(element => {
-        let locationPoint = element.restaurant.location.latitude;
-        console.log(locationPoint);
+// const consume = data => {
+//     data.data.forEach(element => {
+//         let locationPoint = element.restaurant.location.latitude;
+//         console.log(locationPoint);
 
 
-    });
-}
+//     });
+// }
 
 let typeOfFood = document.getElementsByClassName("foodType").value;
 
@@ -59,10 +25,8 @@ window.onload = function mostrarMapa(puntos) {
     };
 
     navigator.geolocation.getCurrentPosition(function(pos) {
-        // const latitud = pos.coords.latitude;
-        // const longitud = pos.coords.longitude;
-        const latitud = -33.4188152;
-        const longitud = -70.64228059999999;
+        const latitud = pos.coords.latitude;
+        const longitud = pos.coords.longitude;
 
         console.log(latitud);
         console.log(longitud);
@@ -72,7 +36,7 @@ window.onload = function mostrarMapa(puntos) {
         const mymap = L.map('map').setView([latitud, longitud], 50);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ3V0ZWZjbyIsImEiOiJjampjdXlqaG0zbXR3M3FvaGU5cG93ZXdlIn0.6b_Z9AeAgi1U7J4wiiX87w', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 17,
+            maxZoom: 15,
             id: 'mapbox.streets',
             accessToken: 'your.mapbox.access.token'
         }).addTo(mymap);
@@ -88,6 +52,8 @@ window.onload = function mostrarMapa(puntos) {
             popupAnchor: [1, -34],
             shadowSize: [41, 41]
         });
+
+
 
         // typeOfFood.forEach(location => {
         //     L.marker([location.latitude, location.longitude], { icon: greenIcon }).addTo(mymap);
